924da5f61c5172ccbf3521eaf528a85bee64a404
Charles Ramcharan <charlesramcharan@yahoo.com>
2021-08-24 17:27:23 -0400

    Initial commit

---
+++ CSharpMortgageCalcMVC/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CSharpMortgageCalcMVC.Models;
+using CSharpMortgageCalcMVC.Helpers;
+
+namespace CSharpMortgageCalcMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult App()
+        {
+            Loan loan = new Loan();
+            loan.Payment = 0.0m;
+            loan.TotalInterest = 0.0m;
+            loan.TotalCost = 0.0m;
+            loan.Rate = 3m;
+            loan.Amount = 25000m;
+            loan.Term = 60;
+
+            return View(loan);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult App(Loan loan)
+        {
+            var loanHelp = new LoanHelper();
+
+            Loan newloan = loanHelp.GetPayment(loan);
+            
+            return View(newloan);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSharpMortgageCalcMVC/Helpers/LoanHelper.cs
@@ -0,0 +1,70 @@
+﻿using CSharpMortgageCalcMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpMortgageCalcMVC.Helpers
+{
+    public class LoanHelper
+    {
+        public Loan GetPayment(Loan loan)
+        {
+
+            //Calculate my monthly payment
+            loan.Payment = CalcPayment(loan.Amount, loan.Rate, loan.Term);
+            //Calculate from one to the term
+            var balance = loan.Amount;
+            var totalInterest = 0.0m;
+            var monthlyInterest = 0.0m;
+            var monthlyPrincipal = 0.0m;
+            var monthlyRate = CalcMonthlyRate(loan.Rate);
+
+            for (int month = 1; month <= loan.Term; month++)
+            {
+                monthlyInterest = CalcMonthlyInterest(balance, monthlyRate);
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                LoanPayment loanpayment = new LoanPayment();
+                loanpayment.Month = month;
+                loanpayment.Payment = loan.Payment;
+                loanpayment.MonthlyPrincipal = monthlyPrincipal;
+                loanpayment.MonthlyInterest = monthlyInterest;
+                loanpayment.TotalInterest = totalInterest;
+                loanpayment.Balance = balance;
+
+                //push the object into the loan model
+                loan.Payments.Add(loanpayment);
+            }
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.Amount + totalInterest;
+            //Calculate a payment schedule
+
+
+            //push payments in the loan
+
+            //return the loan to the view
+            return loan;
+        }
+
+        private decimal CalcPayment(decimal amount, decimal rate, int term)
+        {
+            
+            var monthlyRate = CalcMonthlyRate(rate);
+            var rateD = Convert.ToDouble(monthlyRate);
+            var amountD = Convert.ToDouble(amount);
+            var paymentD = (amountD * rateD) / (1 - Math.Pow(1 + rateD, -term));
+            return Convert.ToDecimal(paymentD);
+        }
+        private decimal CalcMonthlyRate(decimal rate)
+        {
+            return rate / 1200;
+        }
+        private decimal CalcMonthlyInterest(decimal balance, decimal monthlyRate)
+        {
+            return balance * monthlyRate;
+        }
+    }
+}
